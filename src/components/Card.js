1182b4d31cfb5fade0acfb4d0e260fc800b8102a
oliverloops <oliveralr@protonmail.com>
2021-05-22 02:03:36 -0500

    Home layout changes

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,27 +1,30 @@
+import { Link } from "react-router-dom";
 import { Row, Col, Text, Spacer, useTheme } from "@geist-ui/react";
 import styled from "styled-components";
-import python from "../static/python_logo.png";
+import ug from "../static/ug.jpg";
 
 // Styled components
 const Container = styled.div`
-  height: 120px;
+  height: 140px;
   border-radius: 12px;
   border: 3px rgb(218, 221, 225) solid;
 `;
 
 const GroupImg = styled.img`
-  width: 50px;
-  height: 50px;
-  border-radius: 50%;
+  width: 60px;
+  height: 60px;
+  border-radius: 12px;
 `;
 
 const Card = () => {
   const { palette } = useTheme();
 
   return (
-    <Container style={{ backgroundColor: palette.accents_2 }}>
-      <CardContent />
-    </Container>
+    <Link to="/">
+      <Container style={{ backgroundColor: palette.accents_2 }}>
+        <CardContent />
+      </Container>
+    </Link>
   );
 };
 
@@ -30,18 +33,22 @@ const CardContent = () => {
   return (
     <Row gap={0.8}>
       <Col span={6} style={{ padding: "12px" }}>
-        <GroupImg src={python} alt="Group_Logo" />
+        <GroupImg src={ug} alt="Group_Logo" />
       </Col>
       <Col span={20}>
         <Row align="middle">
-          <Text p b size={18}>
+          <Text p b size={19}>
             Título
           </Text>
           <Spacer x={5} />
-          <span>Label</span>
+          <Text p size={14}>
+            Label
+          </Text>
         </Row>
-        <Row align="top">
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <Row style={{ marginTop: "-20px" }} align="top">
+          <Col>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Col>
         </Row>
       </Col>
     </Row>
