0713985657641f5855ad2079c0135278e321475a
oliverloops <oliveralr@protonmail.com>
2021-05-21 19:48:12 -0500

    Home layout changes

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,47 @@
+import { Row, Col, Button } from "@geist-ui/react";
+import styled from "styled-components";
+import me from "../static/me.jpg";
+
+// UI components
+import SearchFilter from "../components/SearchFilter";
+
+// Styled Components
+const Title = styled.h3`
+  padding: 18px 18px 18px 20px;
+  font-size: 1.75rem;
+`;
+
+const ProfileImgContainer = styled.img`
+  border-radius: 50%;
+  width: 50px;
+  height: 50px;
+`;
+
 const Home = () => {
+  const activeButton = () => {
+    console.log("Click!");
+  };
+
   return (
     <>
-      <h1>Hola :3</h1>
+      <Row>
+        <Col span={18}>
+          <Title>Explora</Title>
+        </Col>
+
+        <Row gap={0.7} justify="end">
+          <Col span={28} style={{ padding: "12px" }}>
+            <ProfileImgContainer src={me} alt="profile-photo" />
+          </Col>
+        </Row>
+      </Row>
+      <Row justify="center">
+        <SearchFilter />
+      </Row>
+
+      <Button onClick={() => activeButton()} auto type="success" ghost>
+        Click me!
+      </Button>
     </>
   );
 };
